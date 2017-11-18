80cb87e57eb04323400dd6fbeaf392a131047658
T1m3y <->
2017-11-18 04:26:50 +0100

    did some stuff

--- OurGame/main.cpp
+++ OurGame/main.cpp
@@ -1,9 +1,11 @@
+#pragma once
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
+	sf::CircleShape shape(100.f, 500);
 	shape.setFillColor(sf::Color::Green);
 
 	while (window.isOpen())
@@ -13,6 +15,14 @@ int main()
 		{
 			if (event.type == sf::Event::Closed)
 				window.close();
+
+			if (event.type == sf::Event::KeyPressed)
+				if (event.key.code == sf::Keyboard::A)
+					if(shape.getFillColor() != sf::Color::Green)
+						shape.setFillColor(sf::Color::Red);
+					else shape.setFillColor(sf::Color::Green);
+
+
 		}
 
 		window.clear();
