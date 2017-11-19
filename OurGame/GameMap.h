be8efbcdcbca4c7d762b9c304560864b83364e2c
Vince <Vince@VINCE>
2017-11-20 00:10:19 +0100

    wrote: insertEntity(), removeEntity()

--- OurGame/GameMap.h
+++ OurGame/GameMap.h
@@ -24,21 +24,25 @@ private:
 	typedef decltype(map.size()) Index;
 
 public:
-	typedef sf::Vector2f Position_t;
+	typedef sf::Vector2f Position;
 
 	GameMap(Index sizeX, Index sizeY) : map(Map2DContainer(sizeX, Map1DContainer(sizeY, MapPosContainer())))
 	{
 		
 	}
-	
+
 	void insertEntity(MapEntityRef ent)
 	{
-
+		map.at(ent->getPosition.x).at(ent->getPosition.y).emplace_front(ent);
 	}
 
-	void removeEntity(MapEntity& ent)
+	void removeEntity(MapEntityRef ent)
 	{
+		auto begin = map.at(ent->getPosition.x).at(ent->getPosition.y).begin;
+		auto end = map.at(ent->getPosition.x).at(ent->getPosition.y).end;
 
+		auto entIt = std::find(begin, end, ent);
+		map.at(ent->getPosition.x).at(ent->getPosition.y).erase(entIt);
 	}
 };
 
@@ -52,27 +56,37 @@ private:
 	std::weak_ptr<GameMap> map;
 
 protected:
-	GameMap::Position_t mapPos;
+	GameMap::Position mapPos;
 
 public:
 
 	~MapEntity()
 	{
-		(*map)
+		(*map);
 	}
 
 	enum MapEntityType
 	{
+		Environment,
 		Unit,
-		Building,
-		Environment
+		City,
+		Ressource,
 	};
 
-	MapEntity(std::weak_ptr<GameMap> map, MapEntityType type, GameMap::Position_t pos)
+	MapEntity(std::weak_ptr<GameMap> map, MapEntityType type, GameMap::Position pos)
 	{
 		this->type = type;
 		mapPos = pos;
 	}
 
+	GameMap::Position getPosition()
+	{
+		return mapPos;
+	}
+
+	MapEntityType getType()
+	{
+		return type;
+	}
 
 };
